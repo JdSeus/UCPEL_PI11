341935e19f3ca815d8e3bda073babfdd53f1cc69
Juliano Seus <julianoseusdev@outlook.com>
2022-09-03 15:46:40 -0300

    Work on Models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Curriculo(models.Model):
+    rua = models.CharField(max_length=255, blank=True)
+    bairro = models.CharField(max_length=255, blank=True)
+    cidade = models.CharField(max_length=255, blank=True)
+    estado = models.CharField(max_length=255, blank=True)
+    telefone = models.CharField(max_length=255, blank=True)
+
+class Usuario(models.Model):
+    nome = models.CharField(max_length=255)
+    sobrenome = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    senha = models.CharField(max_length=255)
+    data_criacao = models.DateTimeField(default=timezone.now)
+    curriculo = models.ForeignKey(Curriculo, on_delete=models.DO_NOTHING)
+
