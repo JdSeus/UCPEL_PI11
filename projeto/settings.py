55895c8f1428b9f12e3aa372fbfb059095e5b726
Juliano Seus <julianoseusdev@outlook.com>
2022-09-04 13:42:13 -0300

    Creation of ProjetoIntegrador path

--- projeto/settings.py
+++ projeto/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home.apps.HomeConfig'
+    'projetointegrador.apps.ProjetointegradorConfig',
 ]
 
 MIDDLEWARE = [
